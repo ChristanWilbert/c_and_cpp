dfdbfb03e3bf887c9219e74fd53a661d449a7ad8
Christan Wilbert <cwchristan@gmail.com>
2022-03-09 06:41:28 +0530

    intro programs

---
+++ introprograms.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main(){
+    int n;
+    printf("Enter integer: ");
+    scanf("%d",&n);
+    printf("Entered Integer is %d",n);
+    /*
+    input: 5  output:5
+    input: 9147483647 output: 2147483647
+    input: abcd output: 0
+    */
+}
